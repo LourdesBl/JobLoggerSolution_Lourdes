d3eca9cd42560aa65c43f6c1a84dd51420899c85
lourdes.bloin <lourdes.bloin@aent.com>
2018-06-18 09:15:27 -0300

    Add DI with Autofac

--- JobLogger/Program.cs
+++ JobLogger/Program.cs
@@ -1,18 +1,47 @@
 ﻿using System;
+using Autofac;
 
 namespace JobLogger
 {
 	public class Job
 	{
-		public static void Main()
+		public static void Main(string[] args)
 		{
-			var jobLoggerHelper = new JobLoggerHelper();
-			var jobLoggerRepository = new JobLoggerRepository();
+			var container = ConfigureContainer();
+			var application = container.Resolve<JobLoggerApplication>();
 
-			var jobLoggerDb = new JobLoggerCore(jobLoggerHelper,jobLoggerRepository);
-			jobLoggerDb.GetFromDb();
+			application.Run();
+		}
 
-			var jobLogger = new JobLoggerCore(true, true, true,jobLoggerHelper, jobLoggerRepository);
+		private static IContainer ConfigureContainer()
+		{
+			var builder = new ContainerBuilder();
+
+			// Register all dependencies
+			builder.RegisterType<JobLoggerApplication>().AsSelf();
+			builder.RegisterType<JobLoggerRepository>().As<IJobLoggerRepository>();
+			builder.RegisterType<JobLoggerHelper>().As<IJobLoggerHelper>();
+			
+			return builder.Build();
+		}
+	}
+
+	public class JobLoggerApplication
+	{
+		private readonly IJobLoggerHelper _jobLoggerHelper;
+		private readonly IJobLoggerRepository _jobLoggerRepository;
+
+		public JobLoggerApplication(IJobLoggerHelper jobLoggerHelper, IJobLoggerRepository jobLoggerRepository)
+		{
+			this._jobLoggerRepository = jobLoggerRepository;
+			this._jobLoggerHelper = jobLoggerHelper;
+		}
+		public void Run()
+		{
+			//var jobLoggerDb = new JobLoggerCore(_jobLoggerHelper, _jobLoggerRepository);
+			//jobLoggerDb.GetFromDb();
+
+			var jobLogger = new JobLoggerCore(true, true, true, _jobLoggerHelper, _jobLoggerRepository);
 			jobLogger.LogMessage("Error message", true, false, false);
 			jobLogger.LogMessage("Warning message", false, true, false);
 			jobLogger.LogMessage("Log message", false, false, true);
